2ebbbf5763610f06bbc9d02b3e2eae3a0067218b
Ambuj Kulshreshtha <kulshreshthaak.12@gmail.com>
2022-10-23 01:04:27 +0530

    camera started working

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import pyautogui
 
 cap = cv2.VideoCapture(0)
 
-pyautogui.FAILSAFE=True
+pyautogui.FAILSAFE=False
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(max_num_hands=3)
+hands = mpHands.Hands(max_num_hands=1)
 mpDraw = mp.solutions.drawing_utils
 
 
 while True:
 
-    _, img = cap.read()
+    _, img = cap.read()# The image that we take here is the BGR image.
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.flip(img, 1)
     image_height,image_width,_ = img.shape
@@ -25,29 +25,13 @@ while True:
     result = hands.process(RGBimage)
     if result.multi_hand_landmarks:
        
-        for handLMS,face in result.multi_hand_landmarks,faces:
-            x1 = face.left()
-            y1 = face.top()
-            x2 = face.right()
-            y2 = face.bottom()
-
-            face_frame = img.copy()
-            cv2.rectangle(face_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-            landmarks = predictor(gray, face)
-            landmarks = face_utils.shape_to_np(landmarks)
-            left_blink = blinked(landmarks[36], landmarks[37],
-                             landmarks[38], landmarks[41], landmarks[40], landmarks[39])
-            right_blink = blinked(landmarks[42], landmarks[43],
-                              landmarks[44], landmarks[47], landmarks[46], landmarks[45])
+        for handLMS in result.multi_hand_landmarks:
             # handLMS is the list of the laandmarks contining landmarks coordinates
             handLMS = handLMS.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP]
             y,x = handLMS.y,handLMS.x
             pyautogui.moveTo(x,y,duration=0.25)
-            if(left_blink == 1 or right_blink == 1):
-                click+=1
-            # y=handLMS.y*image_height
-            # x=handLMS.x*image_width
+            y=handLMS.y*image_height
+            x=handLMS.x*image_width
 
             
     cv2.imshow("Image", img)
